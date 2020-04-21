430edc3b2aa2041d177679eee5cfd48ff8291436
ZaneDubya <ZaneDubya@users.noreply.github.com>
2020-04-21 09:41:22 -0500

    Rename GenerateAST -> GenerateGrammar
    Grammar seems more appropriate, as the AST is actually generated by Parser.cs in LoxScript...

---
+++ GenerateGrammar/GrammarCollection.cs
@@ -0,0 +1,16 @@
+﻿namespace GenerateGrammar {
+    internal class GrammarCollection {
+        internal readonly string Name;
+        internal readonly bool VisitorIsTyped;
+        /// <summary>
+        /// description of each type: name of the class followed by : and the list of fields, separated by commas. Each field has a type and name.
+        /// </summary>
+        internal readonly string[] TypeDefs;
+
+        public GrammarCollection(string name, bool visitorIsTyped, string[] typeDefs) {
+            Name = name;
+            VisitorIsTyped = visitorIsTyped;
+            TypeDefs = typeDefs;
+        }
+    }
+}
